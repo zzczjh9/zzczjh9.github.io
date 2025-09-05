ba5da49a7d863b6955b49e7b10757b34d3627ac8
zzczjh9 <zzc040902@gmail.com>
2025-09-05 05:58:47 +0000

    Deploying to gh-pages from @ zzczjh9/zzczjh9.github.io@04b0c48af62bed77f94a06c7599b38373a628ad0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -472,32 +472,18 @@ ninja.data = [{
           window.open("https://github.com/zzczjh9", "_blank");
         },
       },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/zecheng-zhu", "_blank");
+        },
+      },{
         id: 'social-inspire',
         title: 'Inspire HEP',
         section: 'Socials',
         handler: () => {
           window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
         },
       },{
       id: 'light-theme',
